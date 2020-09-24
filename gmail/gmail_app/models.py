bec9b5e86c591065fb3bd3f737a6f5f14a2d099e
asif-django <asifkhan.python@gmail.com>
2020-09-24 21:25:07 +0530

    in models name replace with verbose_name

--- gmail/gmail_app/models.py
+++ gmail/gmail_app/models.py
@@ -5,27 +5,27 @@ from django.db import models
 GENDER = (("Male","M"), ("Female","F"), ("Others", "O"))
 
 class Login(models.Model):
-    user_name = models.CharField(max_length=100, null=True, name="User Name")
-    pass_word = models.CharField(max_length=100, null=True, name="Password")
+    user_name = models.CharField(max_length=100, null=True, verbose_name="User Name")
+    pass_word = models.CharField(max_length=100, null=True, verbose_name ="Password")
     def __str__(self):
         return str(self.user_name)
 
 class Register(models.Model):
-    user_name = models.CharField(max_length=100, null=True, name="User Name")
-    email = models.EmailField(max_length=254, null=True, name="Email")
-    password = models.CharField(max_length=100, null=True, name="password")
-    date_of_birth = models.DateField(null=True, blank=True,name="Date Of Birth")
-    gender = models.CharField(max_length=10, choise=GENDER, default="Male", name="Gender")
-    nationality = models.CharField(max_length=50, null=True, blank=True, name="Nationality")
-    mobile = models.CharField(max_length=15, null=True, blank=True, name="Mobile")
+    user_name = models.CharField(max_length=100, null=True, verbose_name ="User Name")
+    email = models.EmailField(max_length=254, null=True, verbose_name ="Email")
+    password = models.CharField(max_length=100, null=True, verbose_name ="password")
+    date_of_birth = models.DateField(null=True, blank=True,verbose_name ="Date Of Birth")
+    gender = models.CharField(max_length=10, choise=GENDER, default="Male", verbose_name ="Gender")
+    nationality = models.CharField(max_length=50, null=True, blank=True, verbose_name ="Nationality")
+    mobile = models.CharField(max_length=15, null=True, blank=True, verbose_name ="Mobile")
     def __str__(self):
         return str(self.user_name)
 
 class Compose(models.Model):
     from_email = models.ForeignKey(Login, null=True, on_delete=models.CASCADE)
-    to_email = models.EmailField(max_length=50, null=True, name="To")
-    subject = models.CharField(max_length=100, null=True, blank=True, name="Subject")
-    message = models.TextField(max_length=250, blank=True, null=True, name="Message")
-    attach = models.FileField(upload_to="attach", max_length=100, name="Attach")
+    to_email = models.EmailField(max_length=50, null=True, verbose_name ="To")
+    subject = models.CharField(max_length=100, null=True, blank=True, verbose_name ="Subject")
+    message = models.TextField(max_length=250, blank=True, null=True, verbose_name ="Message")
+    attach = models.FileField(upload_to="attach", max_length=100, verbose_name ="Attach")
     def __str__(self):
         return str(self.to_email)
